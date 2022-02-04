adf091f9f8c50e7117752a25dc3e8579189b27cb
kichalo_y <yurii.kichalo@inoxoft.com>
2022-02-04 17:51:24 +0200

    initian commit

---
+++ src/presentation/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const App = () => (
+  <View style={{ flex: 1, backgroundColor: 'lightblue', justifyContent: 'center', alignItems: 'center' }}>
+    <Text style={{ fontSize: 50, fontWeight: 'bold'}}>
+      Breath
+    </Text>
+  </View>
+)
