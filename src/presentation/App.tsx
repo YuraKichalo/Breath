cf618d455f94a22c2b3875163cdbe1c86c32eaf5
kichalo_y <yurii.kichalo@inoxoft.com>
2022-02-22 18:54:46 +0200

    added navigation

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
-import { Dashboard } from 'presentation/screens'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+import * as Screens from 'presentation/screens'
+import { RootStackParamList } from './RootStackParamList'
+import { Routes } from './Routes'
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export const App = () => (
-  <Dashboard />
+  <NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name={Routes.Dashboard}
+        component={Screens.Dashboard}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
 )
